d92b2101327154d7619f801d983cd29f4c8411d5
qhwa <qhwa@163.com>
2014-02-24 17:12:36 +0800

    project initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XthHost::Application.config.secret_key_base = 'e18b6bb43884ec1c2a6132063b6aa2dfed7ea6b1519f712251edf20cbad07deff883b74f760a500a3314382268c2ef0bcc8f0b366d014590b46d2d46fc797819'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XthHost::Application.config.session_store :cookie_store, key: '_XthHost_session'
